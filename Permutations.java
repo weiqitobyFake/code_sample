3199492292f7106e48cc04a68a71d126953345ed
weiqitoby600 <weiqitoby600@users.noreply.github.com>
2014-09-06 16:00:56 -0400

    Create Permutations.java

---
+++ Permutations.java
@@ -0,0 +1,48 @@
+package code_sample;
+
+import java.util.*;
+
+public class Permutation {
+	
+	public static void main(String[] args) {
+		String string1 = "abc";
+		getPermutation(string1);
+	}
+	
+	public static void getPermutation(String string) {
+		if (string == null)
+			return;
+		ArrayList<String> ans = new ArrayList<String>();
+		HashSet<String> helper = new HashSet<String>();
+		if (string.length() < 2) {
+			ans.add(string);
+			System.out.println(ans);
+		}
+		
+		ans.add("");
+		
+		for (int i=0; i<string.length(); i++) {
+			ans = helper(ans, helper, string.charAt(i));
+		}
+		
+		System.out.println(ans);
+	}
+	
+	public static ArrayList<String> helper(ArrayList<String> ans, HashSet<String> helper, char ithChar) {
+		ArrayList<String> temp = new ArrayList<String>();
+		
+		for (int k=0; k<ans.size(); k++) {
+			String curr = ans.get(k);
+			for (int i=0; i<=curr.length(); i++) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(curr);
+				sb.insert(i, ithChar);
+				
+				String solution = sb.toString();
+				if (helper.add(solution))
+					temp.add(solution);
+			}
+		}
+		return temp;
+	}
+}
